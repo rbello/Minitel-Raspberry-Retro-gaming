fd95f3c3344aeae8d2cd65d8fb235a4591ae7ac5
R. BELLO <remi@evolya.fr>
2017-04-02 22:58:13 +0200

    Improve synch script

--- Scripts Raspberry/synch-cloud.py
+++ Scripts Raspberry/synch-cloud.py
@@ -10,24 +10,87 @@
 # This file is a part of the SuperSwag projet.
 # Copyleft 2017 - evolya.fr
 
+# Require PyOTP - The Python One-Time Password Library
+# https://github.com/pyotp/pyotp
+# Install : pip install pyotp
+
+### START OF CONFIGURATION
+
+scan_dir = "./RetroPie/roms/"
+scan_ext = [".srm", ".state", ".dat", ".nv", ".hi", ".hs", ".cfg", ".eep", ".fs"]
+ws_api_key = "base32secret3232"
+ws_url = "https://static.evolya.fr/cloud-superswag/"
+
+### END OF CONFIGURATION
+
 import os
 import hashlib
 import time
+import pyotp
+import requests
+import urllib2
+from urlparse import urlparse
 
-dir = "./RetroPie/roms/"
-ext = [".srm", ".state", ".dat", ".nv", ".hi", ".hs", ".cfg", ".eep", ".fs"]
+# Try to load cache file
+print "STEP 1 - Gather current save state from remote server"
+cache = {}
 
-out = open("./sync.dat", "w")
-out.write("" + str(time.time()) + "\n")
+# Generate OTP
+totp = pyotp.TOTP(ws_api_key)
+otp = str(totp.now())
+print "  One-time password:", otp
 
-for root, directories, filenames in os.walk(dir):
+# Ask for remote cache
+print "  Connecting to", "{uri.scheme}://{uri.netloc}/".format(uri=urlparse(ws_url)), "..."
+r = requests.post("http://bugs.python.org", data={"action": "GetCache", "key": otp})
+if r.status_code != 200:
+	print "Error: unable to get remote cache (" + r.status_code + ")"
+	sys.exit(0)
+
+# Fetch lines from result and parse data
+content = [l.strip() for l in r.content.split("\n")]
+updateTime = ""
+for line in content:
+	if updateTime == "":
+		updateTime = line
+		continue
+	row = line.split(" ", 3)
+	cache[row[0]] = [int(row[1]), row[2]]
+print "  Cache was updated on: ", updateTime
+print "  Found objects in cache: ", len(cache)
+print "  Success!"
+
+# Working vars
+cacheWrite = ""
+changed = False
+count_files = 0
+count_changed = 0
+count_unchanged = 0
+
+# Fetch files in scanned directory, search for configuration/save state files,
+# and do the comparison with stored cache.
+print ""
+print "Step 2 - Fetch local files..."
+for root, directories, filenames in os.walk(scan_dir):
 	for file in filenames:
-		path = os.path.join(root, file)
+		# File name and file extension
 		filename, file_extension = os.path.splitext(file)
-		if file_extension in ext:
-			md5 = hashlib.md5(path).hexdigest()
-			mtime = os.path.getmtime(path)
-			print path, md5, time.ctime(mtime)
-			out.write(md5 + " " + str(mtime) + " " + path + "\n")
-
-out.close()
+		# Avoid ignored files
+		if file_extension not in scan_ext:
+			continue
+		count_files += 1
+		# File full path
+		path = os.path.join(root, file)
+		# File hash
+		md5 = hashlib.md5(path).hexdigest()
+		# File last modification time
+		mtime = os.path.getmtime(path)
+		# Check if file has changed
+		if (md5 not in cache) or (mtime > cache[md5][0]):
+			# Print a log
+			print " ", path, md5, time.ctime(mtime)
+			# Send files:
+			# http://stackoverflow.com/questions/68477/send-file-using-post-from-a-python-script
+			count_changed += 1
+		else:
+			count_unchanged += 1
