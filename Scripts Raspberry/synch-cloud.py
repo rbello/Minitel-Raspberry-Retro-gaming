602410eb6f42f85bdcf00a28006dfa36f147cebd
R. BELLO <remi@evolya.fr>
2017-04-09 16:54:35 +0200

    Console can upload saves to server

--- Scripts Raspberry/synch-cloud.py
+++ Scripts Raspberry/synch-cloud.py
@@ -17,17 +17,16 @@
 # Require Requests API
 # Install : pip install requests
 
-### START OF CONFIGURATION
-
-scan_dir = "./RetroPie/roms/"
-scan_ext = [".srm", ".state", ".dat", ".nv", ".hi", ".hs", ".cfg", ".eep", ".fs"]
 ws_api_key = "LSX2I5BLGSXA4T77"
 ws_url = "https://static.evolya.fr/cloud-superswag/"
 console_name = "Minitel"
 
-### END OF CONFIGURATION
+scan_dir = "./RetroPie/roms/"
+scan_ext = [".state", ".state1", ".state2", ".state3", ".state4", ".state5", ".state6", ".state7", ".state8", ".state9", ".state0", ".dat", ".nv", ".hi", ".hs", ".cfg", ".eep", ".fs", ".sfc"]
 
-import os
+############ END OF CONFIGURATION ############
+
+import os, sys
 import hashlib
 import time
 import pyotp
@@ -35,8 +34,17 @@ import requests
 import urllib2
 from urlparse import urlparse
 
-# Try to load cache file
-print "STEP 1 - Gather current save state from remote server"
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Init cache
 cache = {}
 
 # Generate OTP
@@ -44,10 +52,14 @@ totp = pyotp.TOTP(ws_api_key)
 
 # Ask for remote cache
 print "  Connecting to", "{uri.scheme}://{uri.netloc}/".format(uri=urlparse(ws_url)), "..."
-r = requests.get(ws_url)
-if r.status_code != 200:
-	print "Error: unable to get remote cache (" + r.status_code + ")"
-	sys.exit(0)
+try:
+	r = requests.get(ws_url)
+	if r.status_code != 200:
+		print bcolors.FAIL + "Error: unable to get remote cache (" + r.status_code + ")" + bcolors.ENDC
+		sys.exit(1)
+except Exception as e:
+	print bcolors.FAIL + "Error: unable to get remote cache (" + str(e) + ")" + bcolors.ENDC
+	sys.exit(1)
 
 # Fetch lines from result and parse data
 content = [l.strip() for l in r.content.split("\n")]
@@ -69,10 +81,17 @@ count_files = 0
 count_changed = 0
 count_unchanged = 0
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
 # Fetch files in scanned directory, search for configuration/save state files,
 # and do the comparison with stored cache.
 print ""
-print "Step 2 - Fetch local files..."
+print "Fetch local files..."
 for root, directories, filenames in os.walk(scan_dir):
 	for file in filenames:
 		# File name and file extension
@@ -90,14 +109,15 @@ for root, directories, filenames in os.walk(scan_dir):
 		# Check if file has changed
 		if (md5 not in cache) or (mtime > cache[md5][0]):
 			# Print a log
-			print " ", file, md5, time.ctime(mtime)
 			otp = str(totp.now())
-			# Send files:
-			# http://stackoverflow.com/questions/68477/send-file-using-post-from-a-python-script
+			plateforme = os.path.basename(os.path.dirname(path))
 			with open(path, 'rb') as fd:
-				r = requests.post(ws_url, files={file: fd}, data={'key': otp, 'console': console_name, 'hash': md5, 'mtime': mtime})
-				print r.status_code
-				print r.content
+				print "   Upload: ", file, "(" + sizeof_fmt(os.path.getsize(path)) + ") ..."
+				r = requests.post(ws_url, files={file: fd}, data={'key': otp, 'console': console_name, 'hash': md5, 'mtime': mtime, 'plateforme': plateforme})
+				if r.status_code != 200 and r.status_code != 201:
+					print bcolors.FAIL + "  [FAILURE]", str(r.status_code), r.content, "=>", md5 + bcolors.ENDC
+				else:
+					print "  [SUCCESS]", md5, time.ctime(mtime), str(r.status_code), r.content
 			count_changed += 1
 		else:
 			count_unchanged += 1
--- Synchro/index.php
+++ Synchro/index.php
@@ -1,45 +1,177 @@
 <?php
 
-$secret = 'LSX2I5BLGSXA4T77';
+#    _________                          _________                        
+#   /   _____/__ ________   ___________/   _____/_  _  _______     ____  
+#   \_____  \|  |  \____ \_/ __ \_  __ \_____  \\ \/ \/ /\__  \   / ___\ 
+#   /        \  |  /  |_> >  ___/|  | \/        \\     /  / __ \_/ /_/  >
+#  /_______  /____/|   __/ \___  >__| /_______  / \/\_/  (____  /\___  / 
+#          \/      |__|        \/             \/              \//_____/  
+#
+# This file is a part of the SuperSwag projet.
+# Copyleft 2017 - evolya.fr
 
-$tolerance = 1; // n*30sec clock tolerance
+// Secret key
+$otp_secret = 'LSX2I5BLGSXA4T77';
 
-date_default_timezone_set('Europe/Paris');
+// n*30 sec clock tolerance
+$otp_tolerance = 2;
+
+// Timezone
+$current_timezone = 'Europe/Paris';
+
+############ END OF CONFIGURATION ############
+
+if (!function_exists('http_response_code')) {
+	function http_response_code($code = NULL) {
+		if ($code !== NULL) {
+			switch ($code) {
+				case 100: $text = 'Continue'; break;
+				case 101: $text = 'Switching Protocols'; break;
+				case 200: $text = 'OK'; break;
+				case 201: $text = 'Created'; break;
+				case 202: $text = 'Accepted'; break;
+				case 203: $text = 'Non-Authoritative Information'; break;
+				case 204: $text = 'No Content'; break;
+				case 205: $text = 'Reset Content'; break;
+				case 206: $text = 'Partial Content'; break;
+				case 300: $text = 'Multiple Choices'; break;
+				case 301: $text = 'Moved Permanently'; break;
+				case 302: $text = 'Moved Temporarily'; break;
+				case 303: $text = 'See Other'; break;
+				case 304: $text = 'Not Modified'; break;
+				case 305: $text = 'Use Proxy'; break;
+				case 400: $text = 'Bad Request'; break;
+				case 401: $text = 'Unauthorized'; break;
+				case 402: $text = 'Payment Required'; break;
+				case 403: $text = 'Forbidden'; break;
+				case 404: $text = 'Not Found'; break;
+				case 405: $text = 'Method Not Allowed'; break;
+				case 406: $text = 'Not Acceptable'; break;
+				case 407: $text = 'Proxy Authentication Required'; break;
+				case 408: $text = 'Request Time-out'; break;
+				case 409: $text = 'Conflict'; break;
+				case 410: $text = 'Gone'; break;
+				case 411: $text = 'Length Required'; break;
+				case 412: $text = 'Precondition Failed'; break;
+				case 413: $text = 'Request Entity Too Large'; break;
+				case 414: $text = 'Request-URI Too Large'; break;
+				case 415: $text = 'Unsupported Media Type'; break;
+				case 500: $text = 'Internal Server Error'; break;
+				case 501: $text = 'Not Implemented'; break;
+				case 502: $text = 'Bad Gateway'; break;
+				case 503: $text = 'Service Unavailable'; break;
+				case 504: $text = 'Gateway Time-out'; break;
+				case 505: $text = 'HTTP Version not supported'; break;
+				default:
+					exit('Unknown http status code "' . htmlentities($code) . '"');
+				break;
+			}
+			$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+			header($protocol . ' ' . $code . ' ' . $text);
+			$GLOBALS['http_response_code'] = $code;
+		} else {
+			$code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+		}
+		return $code;
+	}
+}
+
+function error($code, $message) {
+	http_response_code($code);
+	echo $message;
+	exit();
+}
+
+date_default_timezone_set($current_timezone);
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	if (file_exists('.cache')) {
-		include '.cache';
+	if (file_exists('./.cache.php')) {
+		$cache = include './.cache.php';
+		echo filemtime('./.cache.php');
+		foreach ($cache as $game => $data) {
+			$update = array_pop($data['updates']);
+			echo "\n{$update['hash']} {$update['mtime']} {$data['emulator']} {$game}";
+		}
 	}
 	else echo time();
 	exit();
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	
-	if (!array_key_exists('key', $_POST))
-		exit('No key given');
 
+	// Check arguments
+	if (!array_key_exists('key', $_POST))
+		error(401, 'Missing request parameter: authentication password (key)');
+	if (!array_key_exists('console', $_POST))
+		error(400, 'Missing request parameter: console name (console)');
+	if (!array_key_exists('hash', $_POST))
+		error(400, 'Missing request parameter: file checksum (hash)');
+	if (!array_key_exists('mtime', $_POST))
+		error(400, 'Missing request parameter: file modification time (mtime)');
+	if (!array_key_exists('plateforme', $_POST))
+		error(400, 'Missing request parameter: emulator name (plateforme)');
+
+	// Error level
 	error_reporting(E_ALL);
 
+	// Import OTP library
 	require_once 'GoogleAuthenticator.php';
 	$ga = new PHPGangsta_GoogleAuthenticator();
-	//$secret = $ga->createSecret();
-	//$otp = $ga->getCode($secret);
+	//$otp_secret = $ga->createSecret();
+	//$otp = $ga->getCode($otp_secret);
 
-	if (!$ga->verifyCode($secret, $_POST['key'], $tolerance)) {
-		exit('Invalid key');
+	// Check OTP
+	if (!$ga->verifyCode($otp_secret, $_POST['key'], $otp_tolerance))
+		error(403, 'Forbidden: invalid authentication password');
+	
+	// Check uploaded file
+	if (sizeof($_FILES) !== 1)
+		error(400, 'File upload error: no file uploaded');
+	
+	// Extract game data
+	$game = array_shift($_FILES);
+	
+	// Check upload
+	if ($game['error'] !== UPLOAD_ERR_OK)
+		error(500, 'File upload error: ' . $game['error']);
+	
+	// Load cache
+	$cache = array();
+	if (file_exists('./.cache.php')) $cache = include './.cache.php';
+	
+	// Fix hash
+	$_POST['hash'] = preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['hash']);
+	
+	// Create store directory
+	if (!is_dir('./saves/')) mkdir('./saves/');
+	
+	//echo "Received update from " . $_POST['console'] . " game " . $game['name'] . " hash " . $_POST['hash'];
+
+	// Try to move uploaded file to store directory
+	if (!move_uploaded_file($game['tmp_name'], './saves/' . $_POST['hash']))
+		error(500, 'File upload error: unable to move uploaded file');
+
+	// Create save entry
+	$update = true;
+	if (!array_key_exists($game['name'], $cache)) {
+		$cache[$game['name']] = array(
+			'emulator' => $_POST['plateforme'],
+			'updates' => array()
+		);
+		$update = false;
 	}
 	
-	if (!array_key_exists('console', $_POST))
-		exit('No console name given');
+	// Add update entry
+	$cache[$game['name']]['updates'][] = array(
+			'from'  => $_POST['console'],
+			'hash'  => $_POST['hash'],
+			'mtime' => intval($_POST['mtime'])
+		);
 	
-	// $_POST['hash']
-	// $_POST['mtime']
+	// Save cache
+	file_put_contents('./.cache.php', '<?php return ' . var_export($cache, true) . ';');
 
-	$game = @array_shift($_FILES);
-	
-	if ($game['error'] !== 0)
-		exit('File upload error: ' . $game['error']);
-	
-	echo "Received update from " . $_POST['console'] . " game " . $game['name'] . " hash " . $_POST['hash'];
+	http_response_code($update ? 200 : 201);
+	echo 'OK';
+
 }
